1245a555b06dc398c26a0ea1420cb352b634ff1f
Abdulahi-84 <abdulahi.ibrahim@polarisdigitech.net>
2025-06-06 08:05:58 +0100

    Update hr_app.py

--- hr_app.py
+++ hr_app.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import os
 import pandas as pd
 import plotly.express as px
-from fpdf import FPDF # Correct import for fpdf
+from fpdf import FPDF 
 import base64
 
 # --- Configuration & Paths ---
